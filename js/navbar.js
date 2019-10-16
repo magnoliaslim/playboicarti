af2cf6f0e01c73cb006d09628629f75667f99309
magnoliaslim <47787971+magnoliaslim@users.noreply.github.com>
2019-10-16 21:46:14 +0200

    Add files

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+function openNav() {
+	document.getElementById("navbar").classList.remove('hide')
+    document.getElementById("navbar").classList.add('show')
+}
+
+function closeNav() {
+    document.getElementById("navbar").classList.remove('show')
+    document.getElementById("navbar").classList.add('hide')
+}
